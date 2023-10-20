d71a54ae3a71c141133d55c5b2386e02b7af5cea
liminghong.dev <lmh0066@outlook.com>
2023-10-20 14:27:19 +0800

    add fill methods

--- SSNHL/util.py
+++ SSNHL/util.py
@@ -1,21 +1,26 @@
+import math
+
 import numpy as np
 import pandas as pd
+from fancyimpute import KNN
 from sklearn.metrics import auc, roc_curve
 from sklearn.preprocessing import label_binarize
 
+from SSNHL.rpca.extendedRCPA import extendedRPCA
 
-def load_data(file_path):
+
+def load_data(file_path, preprocess_func):
     data = pd.read_excel(file_path, index_col=0, skiprows=[1, 4], header=[0, 1, 2])
     data = data.drop(["note"], axis=1)
-    filter_data = Preprocess().do(data, "default")
 
-    y = np.array(
-        filter_data["efficacy evaluation"][
-            filter_data["efficacy evaluation"].columns[0]
-        ]
-    )
-    filter_data = filter_data.drop(["efficacy evaluation"], axis=1)
+    result_col = ("efficacy evaluation", "Unnamed: 37_level_1", "Unnamed: 37_level_2")
+    data = data.dropna(subset=[result_col])
+    y = data[result_col].copy()
+    data = data.drop(["efficacy evaluation"], axis=1)
+
+    filter_data = Preprocess().do(data, preprocess_func)
     X = np.array(filter_data)
+    y = np.array(y[filter_data.index])
 
     return X, y
 
@@ -24,14 +29,16 @@ class Preprocess:
     def __init__(self):
         self.function_map = {
             "default": self.default,
+            "rpca": self.rpca,
+            "KNN": self.KNN,
         }
-    
+
     def do(self, data: pd.DataFrame, func_name: str):
         filter_data = self.function_map[func_name](data)
 
         for column in filter_data.columns:
             filter_data[column] = pd.to_numeric(filter_data[column])
-        
+
         return filter_data
 
     def _clean_row(self, data: pd.DataFrame, string: str) -> pd.DataFrame:
@@ -56,6 +63,58 @@ class Preprocess:
         filter_data = filter_data.dropna()
         filter_data = self._clean_row(filter_data, "-")
         filter_data = self._clean_row(filter_data, "normal")
+        return filter_data
+
+    def rpca(self, data: pd.DataFrame, iter=10000) -> pd.DataFrame:
+        filter_data = data.copy()
+        filter_data = filter_data.drop(
+            ("coagulation function", "fibrinogen", "after treatment"), axis=1
+        )
+        filter_data = filter_data.drop(
+            ("blood lipids", "Unnamed: 17_level_1", "triglycerides"), axis=1
+        )
+        filter_data = filter_data.drop(
+            ("treatment plan", "Unnamed: 27_level_1", "tympanic injection time"), axis=1
+        )
+
+        filter_data["ear"] = filter_data["ear"].replace(["L", "R", "L/R"], [1, 2, 3])
+
+        filter_data = filter_data.replace("-", np.nan)
+        filter_data = filter_data.replace("normal", np.nan)
+
+        m, n = filter_data.shape
+        omega = np.where(filter_data.notnull() == True)
+        _lambda = 1.0 / math.sqrt(max(m, n))
+
+        fill_input_data = filter_data.copy()
+        fill_input_data = fill_input_data.fillna(1000)
+        rpca_output_data, e, iter, stop_conv = extendedRPCA(
+            fill_input_data, omega, _lambda, 1e-7, iter
+        )
+        filter_data[:] = rpca_output_data
+
+        return filter_data
+
+    def KNN(self, data: pd.DataFrame) -> pd.DataFrame:
+        filter_data = data.copy()
+        filter_data = filter_data.drop(
+            ("coagulation function", "fibrinogen", "after treatment"), axis=1
+        )
+        filter_data = filter_data.drop(
+            ("blood lipids", "Unnamed: 17_level_1", "triglycerides"), axis=1
+        )
+        filter_data = filter_data.drop(
+            ("treatment plan", "Unnamed: 27_level_1", "tympanic injection time"), axis=1
+        )
+
+        filter_data["ear"] = filter_data["ear"].replace(["L", "R", "L/R"], [1, 2, 3])
+
+        filter_data = filter_data.replace("-", np.nan)
+        filter_data = filter_data.replace("normal", np.nan)
+
+        filled_data = KNN(k=10).fit_transform(filter_data)
+        filter_data[:] = filled_data
+
         return filter_data
 
 
